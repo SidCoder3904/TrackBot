f5189c565f5cb119758556483433b38ab2e33d9b
Siddharth Verma <siddharthverma3904@gmail.com>
2023-10-20 18:18:35 +0530

    direction feedback ready

--- tracker.py
+++ tracker.py
@@ -2,7 +2,31 @@ import cv2 as cv
 import mediapipe as mp
 
 mesh = mp.solutions.face_mesh
-live = cv.VideoCapture(0)
+live = cv.VideoCapture(0) # replace 0 by 1 for external webcam
+
+# def moveUp(Y) :
+
+# def moveDown(Y) :
+
+# def moveClock(Y) :
+
+# def moveAntiClock(Y) :
+
+
+def feedback(frame, X, Y) : # instead of 0.5(strict inequality) give minute error lease
+    H, W, Z = frame.shape
+    if X>0.5 :
+        # moveAntiClock(X)
+        cv.arrowedLine(frame, (int(0.7*W), int(0.5*H)), (int((0.7+0.3*(X-0.5)/0.5)*W), int(0.5*H)), (0, 255, 0), 2)
+    else :
+        # moveClock(X)
+        cv.arrowedLine(frame, (int(0.3*W), int(0.5*H)), (int((0.3+0.3*(X-0.5)/0.5)*W), int(0.5*H)), (0, 255, 0), 2)
+    if Y>0.5:
+        # moveDown(Y)
+        cv.arrowedLine(frame, (int(0.5*W), int(0.7*H)), (int(0.5*W), int((0.7+0.3*(Y-0.5)/0.5)*H)), (0, 255, 0), 2)
+    else :
+        # moveUp(Y)
+        cv.arrowedLine(frame, (int(0.5*W), int(0.3*H)), (int(0.5*W), int((0.3+0.3*(Y-0.5)/0.5)*H)), (0, 255, 0), 2)
 
 with mesh.FaceMesh(refine_landmarks=True) as face_mesh :
     while live.isOpened() :
@@ -16,10 +40,11 @@ with mesh.FaceMesh(refine_landmarks=True) as face_mesh :
             if points :
                 marks = points[0].landmark
                 if isFirst :
-                    centerX = int(marks[6].x * W)
-                    centerY = int(marks[6].y * H)
+                    objX = int(marks[6].x*W)
+                    objY = int(marks[6].y*H)
                     isFirst = False
-                cv.circle(image, (centerX, centerY), 5, (0, 255, 0), -1)
+                cv.circle(image, (objX, objY), 5, (0, 255, 0), 1)
+                feedback(image, marks[6].x, marks[6].y)
                 cv.imshow("Display", image)
             if(cv.waitKey(20) & 0xFF == ord('d')) :
                 break
